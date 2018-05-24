6c7f0f8dc7f31cb711d191057bf31e99fb81d5a7
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-05-24 11:46:28 -0700

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,19 @@
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = require('./env');
 
 module.exports = {
-  [process.env.NODE_ENV]: {
+  development: {
     client: 'pg',
     connection: {
       host: PGHOST,
       database: PGDATABASE
     },
+    migrations: { directory: './db/migrations' },
+    seeds: { directory: './db/seeds' }
+  },
+  production: {
+    client: 'pg',
+    connection:
+      'postgres://utydjvlemokilt:2f9d604a8a94e825c9af5954eab449a948923a74d6c2a9cd1688e8d40ea0a607@ec2-54-243-235-153.compute-1.amazonaws.com:5432/d818hs6dt9k922',
     migrations: { directory: './db/migrations' },
     seeds: { directory: './db/seeds' }
   }

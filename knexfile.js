fa3956d352e526ae6d19530f301cf704540cd25f
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-05-24 10:38:44 -0700

    knex file

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = require('./env');
 
 module.exports = {
   [process.env.NODE_ENV]: {
-    client: 'pg',
+    client: 'postgres',
     connection: {
       host: PGHOST,
       database: PGDATABASE
